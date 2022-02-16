c489636ffb9e2db4c8ddd58bacaebca8e4b53927
zeke800 <82127189+zeke800@users.noreply.github.com>
2022-02-16 10:31:34 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
 def return_params():
     #########PARAMETERS START#######
-    yourLat = 46.444   # Your latitude
-    yourLon = 60.444   # Your longitude
+    yourLat = 46.444   # Your latitude (random one used here)
+    yourLon = 60.444   # Your longitude (random one used here)
     yourAlt = 500           # Your altitude (meters above sea level)
     N2YO_api_key = "YOUR-API-KEY"      # Your N2YO API key
     minElevation = -2      # Minimum elevation of the GAC event (can be negative)
